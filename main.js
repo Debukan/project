c34fc42d11f30f506150e63352bfdd48d9c50e42
Debukan <131191342+Debukan@users.noreply.github.com>
2023-09-24 22:57:50 +0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,15 +1,50 @@
-document.addEventListener('DOMContentLoaded', function() {
+const questionContainers = document.querySelectorAll('.question-container');
+const nextButton = document.getElementById('next-button');
 
-    let count = 0;
+let currentQuestionIndex = 0;
 
-    const button1 = document.getElementById('button1');
-    const button2 = document.getElementById('button2');
-
-    button1.addEventListener('click', function() {
-        count++;
+function showQuestion(index) {
+    questionContainers.forEach((container, i) => {
+        if (i === index) {
+            container.style.display = 'block';
+        } else {
+            container.style.display = 'none';
+        }
     });
+}
 
-    button2.addEventListener('click', function() {
-        count++;
-    });
-});
+function loadNextQuestion() {
+    currentQuestionIndex++;
+    if (currentQuestionIndex < questionContainers.length) {
+        showQuestion(currentQuestionIndex);
+    } else {
+        // Последний вопрос - выполните необходимые действия
+        displayResults();
+    }
+}
+
+function displayResults() {
+    let profession = "";
+        if (score >= 8) {
+            profession = "Специалист по искусственному интеллекту";
+        } else if (score >= 6) {
+            profession = "Аналитик данных";
+        } else if (score >= 4) {
+            profession = "Программист";
+        } else if (score >= 2) {
+            profession = "Инженер по кибербезопасности";
+        } else {
+            profession = "Системный администратор";
+        }
+
+    const resultElement = document.getElementById('result');
+    resultElement.textContent = profession;
+    questionContainers[currentQuestionIndex - 1].style.display = 'none';
+    questionContainers[currentQuestionIndex - 1].appendChild(resultsElement);
+
+    nextButton.style.display = 'none';
+
+}
+
+showQuestion(currentQuestionIndex);
+nextButton.addEventListener('click', loadNextQuestion);
