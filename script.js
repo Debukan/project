beaba2403185e717bdb979c56363cc01e93d6043
Debukan <131191342+Debukan@users.noreply.github.com>
2023-12-17 22:46:31 +0300

    Add first test
    Not ready

--- script.js
+++ script.js
@@ -17,25 +17,46 @@ const questionsnum = [
     "Вопрос 5",
     "Вопрос 6",
     "Вопрос 7",
-    "Вопрос 8",
-    "Вопрос 9",
-    "Вопрос 10",
-    "Вопрос 11",    
+    "Вопрос 8"
 ];
 
 const questions = [
-    "Вас интересует администрирование компьютерных систем и сетей?",
-    "Вы предпочитаете создавать новые программы и приложения или улучшать существующие?",
-    "Question 3",
-    "Question 4",
-    "Question 5",
-    "Question 6",
-    "Question 7",
-    "Question 8",
-    "Question 9",
-    "Question 10",
-    "Question 11",
+    "Вам нравится решать логические задачи?",
+    "Вам интересно изучать новые технологии?",
+    "Вы можете сосредоточиться на поставленной задаче?",
+    "Вы терпеливы и настойчивы?",
+    "Вы можете работать самостоятельно и в команде?",
+    "Вы можете быстро учиться новому?",
+    "Вы можете творчески мыслить?",
+    "Вы можете решать нестандартные задачи?"
 ];
+
+const questionsnum2 = [
+    "Вопрос 1",
+    "Вопрос 2",
+    "Вопрос 3",
+    "Вопрос 4",
+    "Вопрос 5",
+    "Вопрос 6",
+    "Вопрос 7",
+    "Вопрос 8",
+    "Вопрос 9",
+    "Вопрос 10"
+]
+
+const questions2 = [
+    "Вам нравится работать с людьми и помогать им решать их проблемы?",
+    "Вы любите изучать новые технологии и разбираться в сложных задачах?",
+    "Вас привлекает творческая работа, включая дизайн, писательство или музыку?",
+    "Вам нравится анализировать данные, решать задачи, опираясь на факты и цифры?",
+    "Вы предпочитаете работать в команде и обсуждать идеи с коллегами?",
+    "Вам нравится работать самостоятельно, без постоянного общения с другими?",
+    "Вам важно иметь свободу выражения собственных идей и видения?",
+    "Вы предпочитаете получать информацию через чтение и самостоятельное изучение?",
+    "Вам нравится помогать и влиять на жизни других людей через свою работу?",
+    "Вас привлекает работа, требующая логического мышления и стратегического планирования?",
+]
+
  
 const changeBtn = document.getElementById('answerButton');
 const question = document.getElementById('changetext');
@@ -44,23 +65,88 @@ const questionnum = document.getElementById('changequest')
 let currentQuestionIndex = 0;
 
 let score = 0;
- 
-changeBtn.addEventListener('click', () => {
-    currentQuestionIndex = (currentQuestionIndex + 1) % questions.length;
-    question.textContent = questions[currentQuestionIndex];
-    questionnum.textContent = questionsnum[currentQuestionIndex];
 
-    const selectedAnswer = document.querySelector('input[type="radio"]:checked')
+if (changeBtn) {
+    changeBtn.addEventListener('click', () => {
+        currentQuestionIndex = currentQuestionIndex + 1;
+        if (currentQuestionIndex >= questions.length) {
+            document.getElementById('question1').style.display = 'none';
+            document.getElementById('result1').style.display = 'block';
+            changeBtn.style.display = 'none';
+            let elements = document.getElementsByClassName("radio_answers");
+            for(let i = 0; i < elements.length; i++) {
+                elements[i].style.display = 'none';
+            }
+            if (score >= 7) {
+                document.getElementById("result1text").textContent = 'Вам подходит IT сфера.';
+                document.getElementById("nextbutton").style.display = 'inline-block';
+            }
+            else if (score >= 4){
+                document.getElementById("result1text").textContent = 'Вам скорее всего подходит IT сфера.';
+                document.getElementById("nextbutton").style.display = 'inline-block';
+            }
+            else {
+                document.getElementById("result1text").textContent = 'Скорее всего вам не подходит IT сфера.';
+            }
+        }
+        else {
+            question.textContent = questions[currentQuestionIndex];
+            questionnum.textContent = questionsnum[currentQuestionIndex];
 
-    if (selectedAnswer === 'yes') {
-        score++;
-    }
+            const selectedAnswer = document.querySelector('input[type="radio"]:checked')
 
-    $("input[type='radio']").prop('checked', false);
+            if (selectedAnswer && selectedAnswer.value == 'yes') {
+                score++;
+            }
 
-    
-});
+            $("input[type='radio']").prop('checked', false);
+        }
+    });
+}
 
+const changeBtn2 = document.getElementById('answerButton2')
+const question2 = document.getElementById('changetext2')
+const questionnum2 = document.getElementById('changequest2')
+
+let currentQuestionIndex2 = 0;
+
+let score2 = 0;
+
+if (changeBtn2) {
+    changeBtn2.addEventListener('click', () => {
+        currentQuestionIndex2 = currentQuestionIndex2 + 1;
+        if (currentQuestionIndex2 >= questions2.length) {
+            document.getElementById('question2').style.display = 'none';
+            document.getElementById('result2').style.display = 'block';
+            changeBtn2.style.display = 'none';
+            let elements = document.getElementsByClassName("radio_answers");
+            for(let i = 0; i < elements.length; i++) {
+                elements[i].style.display = 'none';
+            }
+            if (score2 >= 8) {
+                document.getElementById("result1text2").textContent = 'Аналитическая сфера (аналитик данных, финансист, исследователь).';
+            }
+            else if (score2 >= 6){
+                document.getElementById("result1text2").textContent = 'Творческая сфера (дизайнер, писатель, художник и т.д.).';
+            }
+            else {
+                document.getElementById("result1text2").textContent = 'Техническая сфера (программист, инженер, аналитик и т.д.).';
+            }
+        }
+        else {
+            question2.textContent = questions2[currentQuestionIndex2];
+            questionnum2.textContent = questionsnum2[currentQuestionIndex2];
+
+            const selectedAnswer = document.querySelector('input[type="radio"]:checked')
+
+            if (selectedAnswer && selectedAnswer.value == 'yes') {
+                score++;
+            }
+
+            $("input[type='radio']").prop('checked', false);
+        }
+    });
+}
 
 $("input[type=\"radio\"]").click(function(){
     var value = $(this).val();
